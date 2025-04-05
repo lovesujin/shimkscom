5f72a2cad959acd700a210f5b3b3d5c277ed0d78
shimks.com <parkshim72@outlook.com>
2025-04-05 17:04:43 +0900

    up

--- wp-content/themes/shimksone/header.php
+++ wp-content/themes/shimksone/header.php
@@ -1,19 +1,37 @@
-<html>
+<?php
+/**
+ * The header.
+ *
+ * This is the template that displays all of the <head> section and everything up until main.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?> <?php twentytwentyone_the_html_classes(); ?>>
 <head>
-<title>Custom Theme</title>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'./cssjs/bootstrap.js'; ?>">
-</script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/jquery-ui.min.js'; ?>">
-</script>
-<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri().'/js/bootstrap.js'; ?>">
-</script>
-<link  href="<?php echo get_stylesheet_directory_uri().'/css/bootstrap.css'; ?>">
-<link  href="<?php bloginfo('stylesheet_url'); ?>">
+	<meta charset="<?php bloginfo( 'charset' ); ?>" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<?php wp_head(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#content">
+		<?php
+		/* translators: Hidden accessibility text. */
+		esc_html_e( 'Skip to content', 'twentytwentyone' );
+		?>
+	</a>
 
-<div id="ttr_header" class="jumbotron">
-<h1>HEADER</h1>
-</div>
-<div class="container">
+	<?php get_template_part( 'template-parts/header/site-header' ); ?>
+
+	<div id="content" class="site-content">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main">
--- wp-content/themes/shimksone/index.php
+++ wp-content/themes/shimksone/index.php
@@ -1,20 +1,45 @@
-<?php get_header(); ?>
-<div id="ttr_main" class="row">
-<div id="ttr_content" class="col-lg-8 col-sm-8 col-md-8 col-xs-12">
+<?php
+/**
+ * The main template file
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_One
+ * @since Twenty Twenty-One 1.0
+ */
 
-<div class="row">
-<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-<div class="col-lg-6 col-sm-6 col-md-6 col-xs-12">
-<h1><?php the_title(); ?></h1>
-<h4>Posted on <?php the_time('F jS, Y') ?></h4>
-<p><?php the_content(__('(more...)')); ?></p>
-</div>
-<?php endwhile; else: ?>
-<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+get_header(); ?>
+
+<?php if ( is_home() && ! is_front_page() && ! empty( single_post_title( '', false ) ) ) : ?>
+	<header class="page-header alignwide">
+		<h1 class="page-title"><?php single_post_title(); ?></h1>
+	</header><!-- .page-header -->
 <?php endif; ?>
-</div>
-</div>
-<?php get_sidebar(); ?>
-</div>
 
-<?php get_footer(); ?>
+<?php
+if ( have_posts() ) {
+
+	// Load posts loop.
+	while ( have_posts() ) {
+		the_post();
+
+		get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) );
+	}
+
+	// Previous/next page navigation.
+	twenty_twenty_one_the_posts_navigation();
+
+} else {
+
+	// If no content, include the "No posts found" template.
+	get_template_part( 'template-parts/content/content-none' );
+
+}
+
+get_footer();
